827053dc89624c5342a3bb31d9a37085be4ab685
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-30 14:03:23 -0300

    Resolvendo exercicio 1 da Lista 4

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Iremos contar de 1 a 100");
+        for(int contador = 1; contador <= 100; contador++){
+            System.out.println(contador);
+        }
+
+    }
+
+}
