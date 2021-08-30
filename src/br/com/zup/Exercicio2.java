4d8c45dd12ae38c38a049a30d179b61c061d00e3
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-30 14:07:27 -0300

    Resolvendo o exercicio 2 da Lista 4

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,17 @@
+package br.com.zup;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Vamos realizar a contagem regressiva de 100 a 1");
+        int contador = 100;
+
+        while(contador >= 1){
+            System.out.println(contador);
+            contador--;
+        }
+
+    }
+
+}
